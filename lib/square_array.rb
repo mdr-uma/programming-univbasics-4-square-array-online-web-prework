d9b4814ebad15d19221fec65597b7ed4da20addc
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-29 01:49:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,11 @@
 def square_array(array)
-   counter = 0
 
-   while array[counter] do
-    puts array[counter]
+  new_array = []
+  counter = 0
+
+   while array < array.length do
+    new_array << array[counter] ** 2
     counter += 1
   end
+  returns new_array
 end
